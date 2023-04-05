faa83494eb8e446544a4eb9b4ba972790d813296
unknown <yunjisu01@naver.com>
2023-04-06 00:11:46 +0900

    exit 명령어 나올 ë때까지 계속 입ë력받기

--- src/com/ysj/exam/board/Main.java
+++ src/com/ysj/exam/board/Main.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
     System.out.println("== 게시판 v 0.1  ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
+
+    while (true) {
+      System.out.printf("명령) ");
+      String cmd = sc.nextLine();
+      System.out.printf("입력된 명령어 : %s\n", cmd);
+      if(cmd.equals("exit")){
+        break;
+      }
+    }
+
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
