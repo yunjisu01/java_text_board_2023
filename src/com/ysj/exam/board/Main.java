985aa2023fe86200f8239bf84fd6bff2d5f2356b
unknown <yunjisu01@naver.com>
2023-04-05 23:30:21 +0900

    시작 알림 메시지 출력

--- src/com/ysj/exam/board/Main.java
+++ src/com/ysj/exam/board/Main.java
@@ -3,6 +3,9 @@ package com.ysj.exam.board;
 public class Main {
   public static void main(String[] args) {
 
-    System.out.println("Hello world!");
+    System.out.println("== 게시판 v 0.1  ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("명령)");
+    System.out.println("== 프로그램 종료 ==");
   }
 }
