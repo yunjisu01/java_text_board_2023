d7650ce4e04486f5b2b57e5f23c4bdf33de376bc
unknown <yunjisu01@naver.com>
2023-04-05 23:59:57 +0900

    명령 어 입력받기

--- src/com/ysj/exam/board/Main.java
+++ src/com/ysj/exam/board/Main.java
@@ -1,11 +1,17 @@
 package com.ysj.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
     System.out.println("== 게시판 v 0.1  ==");
     System.out.println("== 프로그램 시작 ==");
     System.out.println("명령)");
+    String cmd = sc.nextLine();
+    System.out.printf("입력된 명령어 : %s\n", cmd);
     System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
   }
 }
