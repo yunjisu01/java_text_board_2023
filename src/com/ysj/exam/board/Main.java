466c04cb8c783bce86c394302defe8b3d6babfcf
unknown <yunjisu01@naver.com>
2023-04-06 00:29:39 +0900

    등록시마다 게시물번호 증가

--- src/com/ysj/exam/board/Main.java
+++ src/com/ysj/exam/board/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
 
     System.out.println("== 게시판 v 0.1  ==");
     System.out.println("== 프로그램 시작 ==");
@@ -13,19 +14,18 @@ public class Main {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")){
+      if (cmd.equals("exit")) {
         break;
-      }
-      else if(cmd.equals("/usr/article/write")){
+      } else if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목: ");
-        String title=sc.nextLine();
+        String title = sc.nextLine();
         System.out.printf("내용: ");
         String body = sc.nextLine();
-        int id =1;
-        System.out.println(id+"번 게시물 등록되었습니다.");
-      }
-      else{
+        articleLastId++;
+        int id=articleLastId;
+        System.out.println(id + "번 게시물 등록되었습니다.");
+      } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
     }
